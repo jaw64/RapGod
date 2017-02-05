66b262eda3c8604d08b42b610e36d06330b9ef2c
Nick Fahey <nickfahey@Nicks-MacBook-Pro.local>
2017-02-04 21:30:35 -0500

    Added bigger test file. Working on retrying lines that fail

--- src/markov/Markov.java
+++ src/markov/Markov.java
@@ -8,15 +8,20 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Set;
 
+import datamuse.Datamuse;
+import datamuse.JSONParse;
+
+import com.json.exceptions.JSONParsingException;
+
 public class Markov {
   private String corpus;
   private Map<String, Map<String, List<String>>> chain = new HashMap<>();
-  private final int LINESIZE = 10;
+  private final int LINESIZE = 8;
+  private int lineCount = 0;
 
   public Markov(String corpus) {
     this.corpus = corpus;
     triples();
-    //System.out.println(chain);
   }
 
   private void triples() {
@@ -40,16 +45,67 @@ public class Markov {
     }
   }
 
-  public String generateLine() {
+  public void generate(int lines) {
+    while (lineCount < lines) {
+      generateLine();
+    }
+  }
+
+  private void generateLine() {
     Random r = new Random();
     String word1 = "*STOP*";
     List<String> secondWords = new ArrayList<>(chain.get(word1).keySet());
     String word2 = secondWords.get(r.nextInt(secondWords.size()));
+    String toRhyme = word2;
+    System.out.println(generateWords(word1, word2));
+    generateRhymeLine(toRhyme);
+    lineCount++;
+  }
+
+  private void generateRhymeLine(String word) {
+    Random r = new Random();
+    List<String> allRhymes = new ArrayList<>();
+    List<String> rhymes = new ArrayList<>();
+    List<String> almostRhymes = new ArrayList<>();
+    try {
+      rhymes = Arrays.asList(JSONParse.parseWords(Datamuse.rhymesWith(word)));
+      allRhymes.addAll(rhymes.subList(0, rhymes.size()/2));
+    } catch (JSONParsingException e){      
+    } finally {
+      try {
+        almostRhymes = Arrays.asList(JSONParse.parseWords(Datamuse.almostRhymesWith(word)));
+        allRhymes.addAll(almostRhymes.subList(0, almostRhymes.size()/2));
+      } catch (JSONParsingException e) {}
+    }
+    allRhymes.addAll(rhymes.subList(rhymes.size()/2, rhymes.size()));
+    allRhymes.addAll(almostRhymes.subList(almostRhymes.size()/2, almostRhymes.size()));
+    String word1 = "*STOP*";
+    for (String rhyme : allRhymes) {
+      if (chain.get(rhyme) != null) {
+        word1 = rhyme;
+        break;
+      }
+    }
+    List<String> secondWords = new ArrayList<>(chain.get(word1).keySet());
+    String word2 = secondWords.get(r.nextInt(secondWords.size()));
+    System.out.println(generateWords(word1, word2));
+  }
+
+  private String generateWords(String word1, String word2) {
+    Random r = new Random();
     StringBuilder genWords = new StringBuilder();
     for (int i = 0; i < LINESIZE; i++) {
-      genWords.insert(0, word1);
+      if (!word1.equals("*START*") && !word1.equals("*STOP*")) {
+        genWords.insert(0, word1);
+      }
       if (chain.get(word1) == null || chain.get(word1).get(word2) == null) {
-        return generateLine();
+        if (i > 4) {
+          return genWords.toString();
+        } else {
+          //lineCount--;
+          generateLine();
+          return "";
+        }
       }
       List<String> thirdWords = chain.get(word1).get(word2);
       String word3 = thirdWords.get(r.nextInt(thirdWords.size()));
@@ -57,7 +113,9 @@ public class Markov {
       word2 = word3;
       genWords.insert(0," ");
     }
-    genWords.insert(0, word2);
+    if (!word2.equals("*START*") && !word2.equals("*STOP*")) {
+        genWords.insert(0, word2);
+      }
     return genWords.toString();
   }
 }
--- src/rapgod/Main.java
+++ src/rapgod/Main.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import java.util.Arrays;
+
 import markov.Markov;
+import datamuse.Datamuse;
+import datamuse.JSONParse;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     String corpus = new String(Files.readAllBytes(Paths.get(args[0])));
-    Markov chain = new Markov(corpus.replace(".", "\n"));
-    System.out.println(chain.generateLine());
+    Markov chain = new Markov(corpus.replace(".", "\n").replaceAll("\\p{P}", ""));
+    chain.generate(10);
   }
 }
